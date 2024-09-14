842c291252886586f9496b74c6912ab5104255fa
J Sanders <sanders.j4@gmail.com>
2024-09-14 03:02:54 -0400

    added windows build script

---
+++ scripts/build.bat
@@ -0,0 +1,19 @@
+@echo off
+REM Default PyInstaller options
+set OPTIONS=--onefile ^
+--name="TimeBuddy" ^
+--icon="resources/app_icon.ico" ^
+--add-data="resources/*;resources" ^
+--clean
+
+REM First to this script determins whether `--noconsole`
+if "%1"=="--noconsole" (
+    REM Include `--noconsole` option in PyInstaller options if provided to this script
+    set OPTIONS=%OPTIONS% --noconsole
+    echo Build with --noconsole option...
+) else (
+    REM Don't include the `--noconsole` option if no passed
+    echo Building without the --noconsole option...
+)
+
+pyinstaller %OPTIONS% timebuddy.py
